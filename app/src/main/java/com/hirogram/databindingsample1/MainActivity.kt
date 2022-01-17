bbd5271abea27c8669470f3ec20c705580bce848
morihiroto <arigatoeggplant@gmail.com>
2022-01-17 22:50:02 +0900

    MainActivityを編集

--- app/src/main/java/com/hirogram/databindingsample1/MainActivity.kt
+++ app/src/main/java/com/hirogram/databindingsample1/MainActivity.kt
@@ -1,44 +1,47 @@
 package com.hirogram.databindingsample1
 
+import android.app.LauncherActivity
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.view.ViewParent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.hirogram.databindingsample1.databinding.ActivityMainBinding
-import com.hirogram.databindingsample1.databinding.ListExampleBinding
-import java.security.AccessControlContext
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val adapter = MainListAdapter(this).apply {
-            add(ListItem("ゴミ出し", Date()))
-            add(ListItem("技術書を書く", Date()))
-            add(ListItem("脱稿する", Date()))
-        }
-        binding.listView.adapter = adapter
-    }
-}
+        val adapter = ExampleAdapter(this)
+        adapter.items = ListOf(
 
-class MainListAdapter(context: Context): ArrayAdapter<ListItem>(context, 0) {
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val binding: ListExampleBinding
-        if (convertView == null) {
-            binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.list_example,
-            parent, false)
-            binding.root.tag = binding
-        } else {
-            binding = convertView.tag as ListExampleBinding
-        }
-        binding.item = getItem(position)
-        return binding.root
+        )
     }
+
+    class ExampleAdapter(context: Context) : RecyclerView.Adapter<Holder>() {
+        var items: List<LauncherActivity.ListItem> = emptyList()
+        private val inflater = LayoutInflater.from(context)
+
+        override fun getItemConunt(): Int = items.size
+
+        override fun onCreatViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            val binding: ListExampleBinding =
+                DataBindingUtil.inflate(inflater, R.layout.list_example, parent, false)
+            return Holder(binding)
+        }
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {
+            holder.binding.item = items[position]
+            holder.binding.executePendingBindings()
+        }
+    }
+
+    class Holder(val binding: ListExampleBinding) : RecyclerView.ViewHolder(binding.root)
+
 }
